02019a8d29d44465da39aae01f083dc658588201
Kalmera74 <kalmera74@gmail.com>
2019-12-14 18:19:47 +0300

    Shoot re-worked

--- TowerMasterPrototip/Assets/Scripts/Shooter.cs
+++ TowerMasterPrototip/Assets/Scripts/Shooter.cs
@@ -6,9 +6,12 @@ public class Shooter : MonoBehaviour
 {
     public GameObject Projectile;
     public GameObject Thrower;
-    private float acc;
-    public float Spos;
-    public float Epos;
+    public float acc;
+    public float side;
+    public float SposY;
+    public float EposY;
+    public float SposX;
+    public float EposX;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +24,29 @@ public class Shooter : MonoBehaviour
         
         if (Input.GetMouseButtonDown(0) )
         {
-            Spos = Input.mousePosition.y;
+            SposY = Input.mousePosition.y;
+            SposX = Input.mousePosition.x;
         }
         else if (Input.GetMouseButton(0))
         {
-            Epos = Input.mousePosition.y;
-            //   acc = -((Spos - Epos) % 100) / 5; ;
-            acc = Spos - Epos;
-            Debug.Log(acc);
+            EposY = Input.mousePosition.y;
+            EposX = Input.mousePosition.x;
+            acc = ((SposY - EposY) /100) *10 ;
+            side=-(((SposX-EposX)/100)*1.5f);
+            //acc = Spos - Epos;
+
+
+
+
             
+
         }
         else if (Input.GetMouseButtonUp(0))
         {
+           
+
             Rigidbody rg = Instantiate(Projectile, new Vector3(Thrower.transform.position.x, Thrower.transform.position.y, Thrower.transform.position.z), Quaternion.identity).GetComponent<Rigidbody>();
-            rg.AddForce(acc, 0, 0, ForceMode.Impulse);
+            rg.AddForce(10, acc, side, ForceMode.Impulse);
         }
 
         
