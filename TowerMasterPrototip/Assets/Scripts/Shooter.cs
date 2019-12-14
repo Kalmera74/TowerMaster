f1bae492974b796bfdca82bc4d684a3d1290c20c
Kalmera74 <kalmera74@gmail.com>
2019-12-14 17:08:39 +0300

    scene design

---
+++ TowerMasterPrototip/Assets/Scripts/Shooter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject Projectile;
+    public GameObject Thrower;
+    private float acc;
+    public float Spos;
+    public float Epos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0) )
+        {
+            Spos = Input.mousePosition.y;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Epos = Input.mousePosition.y;
+            //   acc = -((Spos - Epos) % 100) / 5; ;
+            acc = Spos - Epos;
+            Debug.Log(acc);
+            
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Rigidbody rg = Instantiate(Projectile, new Vector3(Thrower.transform.position.x, Thrower.transform.position.y, Thrower.transform.position.z), Quaternion.identity).GetComponent<Rigidbody>();
+            rg.AddForce(acc, 0, 0, ForceMode.Impulse);
+        }
+
+        
+    }
+}
